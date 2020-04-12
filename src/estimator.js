2e425ec769de9a33cc0c4802f324e0e6b98c5f76
Sthembiso Tshabalala <sthembiso2698@gmail.com>
2020-04-12 17:56:08 +0200

    first

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,22 @@
-const covid19ImpactEstimator = (data) => data;
+
+const covid19ImpactEstimator = (data) => {
+   const input = data;
+   const impact = { currentlyInfected: input.reportedCases * 10 } 
+   const severeImpact= {currentlyInfected: input.reportedCases * 50}
+   const { periodType } = input;
+   const { timeToElapse } = input;
+
+const { region:{ avgDailyIncomeInUSD: dailyIncome } } = input;
+const { region: { avgDailyIncomePopulation: population } } = input;
+  
+impact.infectionsByRequestedTime = impact.currentlyInfected * parseInt(Math.pow(2,timeToElapse))
+
+
+return{
+data: input,
+impact,
+severeImpact
+    };
+};
 
 export default covid19ImpactEstimator;
