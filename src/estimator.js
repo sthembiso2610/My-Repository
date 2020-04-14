6ec40666b26a4081f037fda49fa82cf0d49e496e
Sthembiso Tshabalala <sthembiso2698@gmail.com>
2020-04-14 13:47:40 +0200

    eslint disabled

--- src/estimator.js
+++ src/estimator.js
@@ -1,16 +1,16 @@
 const covid19ImpactEstimator = (data) => {
-        const input = data;
+    const input = data;
      const   impact = { currentlyInfected: input.reportedCases * 10 };
-    const severeImpact = { currentlyInfected: input.reportedCases * 50 };
+    const severeImpact = { currentlyInfected: input.reportedCases * 50 }
     const { periodType } = input;
     const { timeToElapse } = input;
     impact.infectionsByRequestedTime = impact.currentlyInfected * (2 ** timeToElapse);
 
-    return estimator ({
+    return {
         data: input,
         impact: {},
         severeImpact: {}
-    });
+    };
 },
 
   export default covid19ImpactEstimator;
