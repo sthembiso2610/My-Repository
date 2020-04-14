32c970031e6b29204dee35709c2cee0224824d42
Sthembiso Tshabalala <sthembiso2698@gmail.com>
2020-04-14 12:00:17 +0200

    error fixed

--- src/estimator.js
+++ src/estimator.js
@@ -1,18 +1,16 @@
 const covid19ImpactEstimator = (data) => {
-    const input = data,
-    impact = { currentlyInfected: input.reportedCases * 10 }
-    const severeImpact = { currentlyInfected: input.reportedCases * 50 }
+        const input = data;
+     const   impact = { currentlyInfected: input.reportedCases * 10 };
+    const severeImpact = { currentlyInfected: input.reportedCases * 50 };
     const { periodType } = input;
     const { timeToElapse } = input;
     impact.infectionsByRequestedTime = impact.currentlyInfected * (2 ** timeToElapse);
- 
 
- 
-return {
+    return estimator ({
         data: input,
         impact: {},
         severeImpact: {}
-    },
+    });
 },
 
-export default covid19ImpactEstimator;
+  export default covid19ImpactEstimator;
